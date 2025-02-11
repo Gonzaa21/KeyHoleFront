a9302516a9ae20bef887bb6cb00d89d1805daa3f
Gonzaa21 <gonzidibujante@gmail.com>
2025-02-11 10:58:52 -0300

    Adding docs component

--- src/app/dashboard/docs/page.jsx
+++ src/app/dashboard/docs/page.jsx
@@ -1,6 +1,42 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+
 function Documentation() {
     return (
-        <div className="">Documentaicon</div>
+        <Card>
+            <CardHeader>
+                <CardTitle>Documentation</CardTitle>
+                <CardDescription>How to use and information</CardDescription>
+                <Separator/>
+            </CardHeader>
+            <CardContent>
+                <p>An application designed to help users manage their personal finances efficiently. With this plataform, the users can register their incomes and expenses, investments and objectives</p>
+            </CardContent>
+            <CardFooter className="flex flex-wrap md:flex-nowrap">
+                <h1 className="font-semibold text-xl leading-none tracking-tight pb-3 md:pb-0 md:pr-3">Getting started</h1>
+                <p>To start using this web, browse between categories and complete with appropiate information and that's it! When you return to this website the data will have been saved locally.</p>
+            </CardFooter>
+            <CardHeader>
+                <CardTitle>Principal Content</CardTitle>
+                <Separator/>
+            </CardHeader>
+            <CardContent>
+            <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                <li>Objectives: list of your goals to reach</li>
+                <li>Calendar: Write down important dates</li>
+                <li>Incomes/spent: manage your income and expenses</li>
+                <li>Investments: manage your investments</li>
+            </ul>
+            </CardContent>
+        </Card>
+
     )
 }
 
